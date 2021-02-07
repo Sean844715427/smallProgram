fb18e2cf18962ab41a19a8cdcc8405ecc9eb182e
SeangWang844715427 <1451912940@qq.com>
2021-02-07 15:17:49 +0800

    update

--- pages/music/index.js
+++ pages/music/index.js
@@ -1,4 +1,4 @@
-// pages/test/index.js
+// pages/music/index.js
 Page({
 
   /**
@@ -8,25 +8,12 @@ Page({
 
   },
 
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function (options) {
-    var that = this;
-    /**生成背景星星-开始 */
-    var stars = 400;
-    var r = 0;
-    var stararray = [];
-    for (var i = 0; i < stars; i++) {
-    var s = 0.2 + Math.random() * 1;
-    var curR = r + Math.random() * 300;
-    var rotateY = Math.random() * 360;
-    var rotateX = Math.random() * -50;
-    stararray[i] = { curR: curR, rotateY: rotateY, rotateX: rotateX, s: s };
-    }
-    // console.log(stararray)
-    this.setData({
-    stararray: stararray,
-    })
-    /**生成背景星星-结束 */
-    },
+
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
